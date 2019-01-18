733141c6e6d68a51c227e010707dbe12a67174a2
brendadev <brenda@koneksahealth.com>
2019-01-17 22:14:22 -0500

    renamed file added link to pen

--- js/string_permutation_of_palindrome.js
+++ js/string_permutation_of_palindrome.js
@@ -1,3 +1,4 @@
+// https://codepen.io/thehme/pen/YdMgyw
 // Palindrome Permutation: Given a string, write a function to check if it is a 
 // permutation of a palindrome. A palindrome is a word or phrase that is the same 
 // forward or backwards. A permutation is a rearrangement of letter. The palindrome does not
